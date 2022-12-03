f31b476c772042997f9237c29544d502e8b9963c
AkiraCharles <charles.akira@gmail.com>
2022-12-03 11:17:06 -0500

    changes to test merging

--- another.py
+++ another.py
@@ -1,1 +1,2 @@
 print("I am on a new Branch")
+print("We will try to merge me to the master branch soon")
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
 print("Hello Akira, This is your new Branch File")
+print("I am one commit ahead of the master branch")
+print("We will try to commit me to the master branch soon")
