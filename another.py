fa5bfca686e5d375f7b42f90e5d22ad43c2b38cf
AkiraCharles <charles.akira@gmail.com>
2022-12-03 11:59:38 -0500

    commit all to latest branch

--- another.py
+++ another.py
@@ -1,2 +1,2 @@
-print("I am on a new Branch")
+print("I am on the LATEST Branch")
 print("We will try to merge me to the master branch soon")
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-print("Hello Akira, This is your new Branch File")
+print("Hello Akira, This is your LATEST Branch File")
 print("I am one commit ahead of the master branch")
 print("We will try to commit me to the master branch soon")
